01c43e06b3dbffca3ec782f2b54181c7f553afc0
Elad Kadosh <eladelik@mail.tau.ac.il>
2022-05-19 12:18:15 +0300

    update 19/05

--- DoubleHashTable.java
+++ DoubleHashTable.java
@@ -7,13 +7,13 @@ public class DoubleHashTable extends OAHashTable {
 	public DoubleHashTable(int m, long p) {
 		super(m);
 		this.h1 = ModHash.GetFunc(m,p);
-		this.h2 = ModHash.GetFunc(m,p);
+		this.h2 = ModHash.GetFunc(m-1,p);
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
-		int index = (this.h1.Hash(x)+i*this.h2.Hash(x))% h1.m;
-		return index;
+		long result = this.h1.Hash(x) + (long) i * ((this.h2.Hash(x) + 1));
+		return (int) (result % h1.m);
 	}
 	
 }
